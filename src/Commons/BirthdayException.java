7c08a1160a784462d0d3f14fa2af49f62c6f1966
Shinya9082 <anhtuandtu1993@gmail.com>
2019-12-02 10:34:05 +0700

    Update Birthday Exception

--- src/Commons/BirthdayException.java
+++ src/Commons/BirthdayException.java
@@ -1,15 +1,25 @@
 package Commons;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class BirthdayException extends Exception{
     public BirthdayException(String mes){
         super(mes);
     }
     public static String validateBirthday(String str) throws BirthdayException{
         String regex= "^(0[1-9]|[1-2][0-9]|3[0-1])/(0[1-9]|1[0-2])/(?=19|20[01]\\d)\\d{4}$";
-        if(str.matches(regex)){
+
+        if(str.matches(regex)&&checkYear(str)){
             return str;
         }else {
-            throw new BirthdayException("Invalid Birthday. Please enter again");
+            throw new BirthdayException("Invalid Birthday.Customer must be 18+. Please enter again");
         }
+    }
+    private static boolean checkYear(String str){
+        LocalDate  curTime= LocalDate.now();
+        int currentYear = Integer.parseInt(curTime.toString().substring(0,4)) ;
+        int temp=Integer.parseInt(str.substring(6));
+        return temp <= currentYear - 18;
     }
 }
