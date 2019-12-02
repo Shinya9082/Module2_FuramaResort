f6eaa72037a385a5de9c8e4b0faeb2d146508a20
Shinya9082 <anhtuandtu1993@gmail.com>
2019-12-02 10:46:42 +0700

    Update Name comparator

--- src/Commons/CustomerNameComparator.java
+++ src/Commons/CustomerNameComparator.java
@@ -9,4 +9,7 @@ public class CustomerNameComparator implements Comparator<Customer> {
     public int compare(Customer o1, Customer o2) {
         return o1.getCustomerName().compareTo(o2.getCustomerName());
     }
+    private int yearComparator(Customer o1, Customer o2){
+        return o1.getDayOfBirth().substring(6).compareTo(o2.getDayOfBirth().substring(6));
+    }
 }
